1fe6bba4b8259cf9440296a36d76ab6e7d2ac006
MohOtaki <mohotaki@hotmail.com>
2020-11-23 00:03:25 +0000

    lane detection code uptade

--- Lane Detection Code/main.py
+++ Lane Detection Code/main.py
@@ -3,13 +3,21 @@ import numpy as np
 
 
 def make_coordinates(image , line_parameter):
-    slope, intercept = line_parameter
-    y1= image.shape[0]
-    y2 = int(y1*(3/5))
+    try:
+     slope, intercept = line_parameter
+    except TypeError:
+        slope,intercept =-0.5,0.1
+    y1= 640
+    y2 = int(y1*(0.75))
     x1= int((y1-intercept)/slope)
     x2= int((y2-intercept)/slope)
+    if x1 > 2500 or x1 < -2500:
+        x1 = 319
+
+    if x2 > 2500 or x2 < -2500:
+        x2 = 604
     return np.array([x1,y1,x2,y2])
-def average_slope_intercept(image,line):
+def average_slope_intercept(image, line, backup=None, x11=None):
     left_fit = []
     right_fit = []
     for line in lines:
@@ -21,7 +29,6 @@ def average_slope_intercept(image,line):
             left_fit.append((slope, intercept))
         else:
             right_fit.append((slope,intercept))
-
     left_fit_average = np.average(left_fit,axis=0)
     right_fit_average = np.average(right_fit,axis=0)
     left_line = make_coordinates(image,left_fit_average)
@@ -37,8 +44,8 @@ def canny(image):
 
 
 def region_of_interest(image):
-    height = image.shape[0]
-    polygons = np.array([[(280, height), (1100, height), (580, 250)]])
+    height = 800
+    polygons = np.array([[(0, height), (1500, height), (700, 420)]])
     mask = np.zeros_like(image)
     cv2.fillPoly(mask, polygons, 255)
     maskedImage = cv2.bitwise_and(image, mask)
@@ -49,8 +56,13 @@ def displayLines(image, lines):
     line_image = np.zeros_like(image)
     if lines is not None:
         for x1, y1, x2, y2 in lines:
-            cv2.line(line_image, (x1, y1), (x2, y2), (255, 0, 0), 8)
 
+            if x1 > 10000 or x1 < -10000:
+                x1 = 1000
+            if x2 > 10000 or x2 < -10000:
+                x2 = 1000
+
+            cv2.line(line_image, (x1, y1), (x2, y2), (0, 255, 0), 8)
         return line_image
 
 
@@ -65,14 +77,14 @@ def displayLines(image, lines):
 # cv2.imshow("after", comboImage)
 # cv2.waitKey(0)
 
-cap = cv2.VideoCapture("test2.mp4")
+cap = cv2.VideoCapture("test3.mp4")
 while (cap.isOpened()):
     _, frame = cap.read()
     gray = canny(frame)
     croppedImage = region_of_interest(gray)
-    lines = cv2.HoughLinesP(croppedImage, 2, np.pi / 180, 100, np.array([]), minLineLength=40, maxLineGap=5)
+    lines = cv2.HoughLinesP(croppedImage, 3, np.pi / 180, 50, np.array([]), minLineLength=40, maxLineGap=20)
     average_lines = average_slope_intercept(frame,lines)
     line_image = displayLines(frame, average_lines)
-    comboImage = cv2.addWeighted(frame, 0.3, line_image, 1, 1)
-    cv2.imshow("after", croppedImage)
+    comboImage = cv2.addWeighted(frame, 0.5, line_image, 1, 1)
+    cv2.imshow("after", comboImage)
     cv2.waitKey(1)
