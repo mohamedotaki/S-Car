3e5d47f96af520b1ca5da607e5a78231de780701
MohOtaki <mohotaki@hotmail.com>
2021-01-20 06:59:01 +0000

    database code update

--- database Code.sql
+++ database Code.sql
@@ -2,22 +2,41 @@ USE SCar
 ;
 drop TABLE if exists login
 ;
-drop TABLE if exists otherUsers
+drop TABLE if exists owners
+;
+drop TABLE if exists drivers
 ;
 
 CREATE  table login (
-	id Int NOT NULL AUTO_INCREMENT,
-	email varchar (20) NOT NULL ,
-	password varchar (20) NOT NULL ,
-	carNo varchar (20) NOT NULL ,
-	constraint login_id primary key (id)
+	loginId INT AUTO_INCREMENT,
+		ownerId Int NOT NULL,
+	email VARCHAR (100) NOT NULL ,
+	password VARCHAR (100) NOT NULL ,
+	constraint fk_login foreign key (ownerId)
+		references owners (ownerId),
+	constraint login_id primary key (loginId)
+) 
+;
+CREATE  table owners (
+	ownerId int AUTO_INCREMENT,
+	fullName VARCHAR (100) NOT NULL ,
+	phoneNumber VARCHAR (100) NOT NULL ,
+	carNumber VARCHAR (100) NOT NULL ,
+	keyNo VARCHAR (100) NOT NULL,
+	constraint pk_ownerId PRIMARY key (ownerId)
 ) 
 ;
 
-CREATE  table otherUsers (
-	id Int NOT NULL AUTO_INCREMENT,
-	name varchar (20) NOT NULL ,
-	password varchar (20) NOT NULL ,
-	constraint otherUsers_id PRIMARY key (id)
+CREATE  table drivers (
+	driverId  int AUTO_INCREMENT,
+		ownerId Int NOT NULL,
+	fullName varchar (20) NOT NULL ,
+	emailAddress varchar (20) NOT NULL ,
+	phoneNumber varchar (20) NOT NULL ,
+	admin BOOLEAN NOT NULL,
+	allowedTime  DATE,
+	constraint fk_drivers foreign key (ownerId)
+		references owners (ownerId),
+	constraint pk_drivers PRIMARY key (driverId)
 ) 
 ;
