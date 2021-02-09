bd738d1489884c3b384269d295943e94b7da0d28
MohOtaki <mohotaki@hotmail.com>
2021-02-09 02:53:42 +0000

    updated bluetooth code
    the new code can send the ultrasonic sensor values to the app

---
+++ SCar-ESB32_code/Bluetooth.ino
@@ -0,0 +1,70 @@
+void bluetooth(){
+ if (SerialBT.available()) {
+  switch(SerialBT.read()){
+    case 'C':
+    boolean inControlCar = true;
+    while(inControlCar){
+      Serial.println("in while loop");
+      
+      uint8_t a[8] ={ highByte(ultrasonicValue(frontSensor)),lowByte(ultrasonicValue(frontSensor)),
+                      highByte(ultrasonicValue(backSensor)),lowByte(ultrasonicValue(backSensor)),
+                      highByte(ultrasonicValue(rightSensorF)),lowByte(ultrasonicValue(rightSensorF)),
+                      highByte(ultrasonicValue(rightSensorF)),lowByte(ultrasonicValue(rightSensorF))};
+
+      SerialBT.write(a,8);
+
+
+      
+     if (SerialBT.available()) {
+       switch(SerialBT.read()){
+        case 'L':         // turn left 
+        BTSteering = BTSteering+100;
+         if(BTSteering >=1900){
+          BTSteering=1900;
+           }
+         rightServo(BTSteering);
+         Serial.println(BTSteering);
+         break;
+      case 'R':         // Turn Right
+      BTSteering = BTSteering-100;
+      if(BTSteering <=1100){
+        BTSteering= 1100;
+      }
+      rightServo(BTSteering);
+      Serial.println(BTSteering);
+      break;
+      case 'F':         // go forward or increase the speed
+      Serial.println("forward");
+      break;
+      case 'B':         //  reverse or increase the speed
+      Serial.println("reverse");
+      break;
+      case 'S':         // Stop motor
+      Serial.println("stop");
+      //vTaskResume(wifiTaskHandle);
+      break;
+      case 'P':         // go forward or increase the speed
+      Serial.println("parking assist");
+       //parkingAssist();
+      break;
+      case 'W':         // go forward or increase the speed
+      Serial.println("Wifi");
+       //parkingAssist();
+      break;
+      case 'E':         // go forward or increase the speed
+      inControlCar = false;
+      Serial.println("Exit");
+      break;
+    }
+     }
+     delay(20);
+      
+    }
+    Serial.println("Out");
+  }
+ }
+
+
+   
+  
+}
